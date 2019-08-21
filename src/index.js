b0b64361bf9dea6b0f57e49c37e9487726ede36e
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-08-21 14:50:42 -0500

    Add App component for rendering

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
 ReactDOM.render(
-  <Hello greeting={'Hello' + 'React Ninja'} />,
+  <div>
+    <App />
+  </div>,
   document.getElementById('root')
 );
 
