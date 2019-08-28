9dcd50c9ff4d742698d2ccfe090123ca7772be12
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-08-28 14:24:03 -0500

    Add Provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
 
+const store = createStore(searchRobots);
+
 ReactDOM.render(
-  <div>
+  <Provider store={store}>
     <App />
-  </div>,
+  </Provider>,
   document.getElementById('root')
 );
 
