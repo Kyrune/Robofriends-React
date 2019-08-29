63db1bcfaf559d0068539e4e5e2cf0c32ca8060b
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-08-29 15:54:51 -0500

    Import combineReducers

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
+import thunkMiddleWare from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
 const logger = createLogger();
 
-// Create store/state for searching robots
-const store = createStore(searchRobots, applyMiddleware(logger));
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+// Create store/state for searching/requesting robots
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleWare, logger)
+);
 
 ReactDOM.render(
   <Provider store={store}>
