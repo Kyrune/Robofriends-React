2739843295ca05ab4cc7367b622ccfaf1e5bea9e
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-09-11 15:31:26 -0500

    Updates

---
+++ precache-manifest.1be056c804f1eefc8d0803fc6ac29758.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727c152fcb4ad208ec3157acd7ca97bc",
+    "url": "/Robofriends-React/index.html"
+  },
+  {
+    "revision": "63e9ce712fd0c9d076ed",
+    "url": "/Robofriends-React/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "5689fb13d03b39cd023a",
+    "url": "/Robofriends-React/static/css/main.a79fdb9a.chunk.css"
+  },
+  {
+    "revision": "63e9ce712fd0c9d076ed",
+    "url": "/Robofriends-React/static/js/2.cb51ace9.chunk.js"
+  },
+  {
+    "revision": "5689fb13d03b39cd023a",
+    "url": "/Robofriends-React/static/js/main.0335b146.chunk.js"
+  },
+  {
+    "revision": "b1afb4c46a9f3b9f6d20",
+    "url": "/Robofriends-React/static/js/runtime~main.6794707c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends-React/static/media/SEGA.12a3db8b.woff"
+  }
+]);
